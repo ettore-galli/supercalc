6d8e79b498c8e49321392b39ac9fa4355b26e1ec
Ettore Galli <ettore.galli@gmail.com>
2022-10-15 15:03:11 +0200

    Recycle and reset project for new development

--- src/BackEnd/SuperCalcConstants.js
+++
@@ -1,8 +0,0 @@
-class SuperCalcConstants{
-    static __SAVING_STATUS_UNDEFINED = "*";
-    static __SAVING_STATUS_NEEDED = "N";
-    static __SAVING_STATUS_PROGRESS = "P";
-    static __SAVING_STATUS_DONE = "D";
-}
-
-export default SuperCalcConstants;
